1ca059496a00bd17eb1f6536a2672605aeb2aab9
Jahid Hasan <slniloy500@gmail.com>
2021-06-05 23:40:11 +0600

    add contact page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
-window.screenY(500, () =>{
-    console.log('object');
-})
+$(document).ready(function(){
 
+    $("#toggleBtn1").click(function(){
+      $("#toggleText1").slideToggle();
+    });
+    $("#toggleBtn2").click(function(){
+      $("#toggleText2").slideToggle();
+    });
+    $("#toggleBtn3").click(function(){
+      $("#toggleText3").slideToggle();
+    });
+    $("#toggleBtn4").click(function(){
+      $("#toggleText4").slideToggle();
+    });
+    $("#toggleBtn5").click(function(){
+      $("#toggleText5").slideToggle();
+    });
+    $("#toggleBtn6").click(function(){
+      $("#toggleText6").slideToggle();
+    });
+
+  });
